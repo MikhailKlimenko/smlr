5aa602f9f62f6a93a4c6ad51e2d6ea9c24646374
kmgkmgkmg <kmgkmgkmg@mail.ru>
2018-06-13 21:55:14 +0300

    create repository

---
+++ src/main/kotlin/ru/fnnetrolle/sml/SmlrApplication.kt
@@ -0,0 +1,11 @@
+package ru.fnnetrolle.sml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmlrApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmlrApplication>(*args)
+}
